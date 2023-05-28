6f7fbd1f9d263ce08fdc7946652f53ef47c69d88
Mushbt <>
2023-05-28 10:22:09 +0000

    Test

--- HELLO_DJANGO/settings.py
+++ HELLO_DJANGO/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+if os.path.isfile("env.py"):
+    import env
 from pathlib import Path
+import os
+import dj_database_url
+import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +81,7 @@ WSGI_APPLICATION = 'HELLO_DJANGO.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
